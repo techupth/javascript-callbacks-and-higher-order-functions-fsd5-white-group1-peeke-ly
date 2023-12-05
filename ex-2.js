e6b23bf9ba605b5b7002fa4a0b999ecac937a12a
Gravizz <peeke0rr@gmail.com>
2023-12-05 13:45:08 +0700

    feat(ex-2):complete exercise

--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,21 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  if (operation(array) >= 5) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function counter(array) {
+  let counter = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > 70) {
+      counter++;
+    }
+  }
+  return counter;
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -10,6 +25,10 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, counter);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, counter);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, counter);
+
+console.log(scoreRoom1Result);
+console.log(scoreRoom2Result);
+console.log(scoreRoom3Result);
